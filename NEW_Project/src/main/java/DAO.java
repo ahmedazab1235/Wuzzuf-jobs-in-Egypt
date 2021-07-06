973807cd588744a1b4e48d7ed73c8ba31ffb234c
Ahmed Essam Azab <mac@macs-MacBook-Pro.local>
2021-07-06 16:18:39 +0200

    This is the first commit

---
+++ NEW_Project/src/main/java/DAO.java
@@ -0,0 +1,35 @@
+
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.sql.DataFrameReader;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+
+
+public class DAO
+{
+
+    public Dataset<Row> readCSV(String fileName)
+    {
+        Logger.getLogger("org").setLevel(Level.ERROR);
+        final SparkSession sparkSession = SparkSession.builder ().appName ("Spark CSV Analysis Demo").master ("local[2]")
+                .getOrCreate ();
+        // Get DataFrameReader using SparkSession
+        final DataFrameReader dataFrameReader = sparkSession.read ();
+        dataFrameReader.option ("header", "true");
+        final Dataset<Row> csvDataFrame = dataFrameReader.csv (fileName);
+        // Print Schema to see column names, types and other metadata
+
+
+        return csvDataFrame;
+
+
+
+
+    }
+
+}
